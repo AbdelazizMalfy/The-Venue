f750ff43418532e32fe6d28c0926d5d56ce7e892
Abdelaziz <abdelazizMalfy@gmail.com>
2019-06-12 15:30:16 +0200

    toolbar Added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./resources/styles.css";
-import Header from './components/Header';
+import Header from './components/header_footer/Header';
 
 function App() {
   return (
--- src/components/header_footer/Header.js
+++ src/components/header_footer/Header.js
@@ -5,14 +5,36 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/core/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
+import SideDrawer from './SideDrawer';
 
 class Header extends Component {
+    state = {
+        drawerOpen: false,
+        headerShow: false
+    }
+    
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll);
+    }
+
+    handleScroll = () => {
+
+        if(window.scrollY > 0){
+            this.setState({headerShow:true});
+        }
+    }
+
+    toggoleDrawer = (value) => {
+        this.setState({drawerOpen: value})
+    }
+
+
     render() {
         return (
             <AppBar 
             position="fixed"
             style={{
-                backgroundColor:"#2f2f2f",
+                backgroundColor: this.state.headerShow ? "#2f2f2f" : 'transparent',
                 boxShadow: 'none',
                 padding: "10px 0px" 
             }}  >
@@ -26,10 +48,15 @@ class Header extends Component {
                     <IconButton
                         aria-label="Menu"
                         color="inherit"
-                        onClick={() => console.log('open')}
+                        onClick={() => this.toggoleDrawer(true)}
                     >
                         <MenuIcon/>
                     </IconButton>
+
+                    <SideDrawer
+                        open={this.state.drawerOpen}
+                        onClose={(value) => this.toggoleDrawer(value)}
+                    />
 
                 </Toolbar>
             </AppBar>
